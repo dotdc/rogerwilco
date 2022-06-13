c4897acf59ebb846d0316f3e76dc6bcf632714b7
David <davidcalvertfr@gmail.com>
2022-06-13 19:21:16 +0200

    feat: use gorilla/mux and returncodes
    Signed-off-by: David <davidcalvertfr@gmail.com>

--- main.go
+++ main.go
@@ -9,36 +9,47 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var AppVersion = "unset"
 
 type Page struct {
-	Title   string
-	Version string
+	Title     string
+	Version   string
+	UserAgent string
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	values := Page{
-		Title:   "Roger Wilco App",
-		Version: AppVersion,
+		Title:     "Roger Wilco App",
+		Version:   AppVersion,
+		UserAgent: r.UserAgent(),
 	}
 
 	template, err := template.ParseFiles("./templates/index.html")
 	if err != nil {
-		log.Println("Error parsing files :", err)
+		rc := http.StatusInternalServerError
+		w.WriteHeader(rc)
+		log.Println(r.URL.Path, rc, "[ERROR] parsing files:", err)
+		return
 	}
 
 	terr := template.Execute(w, values)
 	if terr != nil {
-		log.Println("Error executing template :", terr)
+		returncode := http.StatusInternalServerError
+		w.WriteHeader(returncode)
+		log.Println(r.URL.Path, returncode, "[ERROR] executing template:", terr)
 		return
 	}
+
+	log.Println(r.URL.Path, http.StatusOK, "[INFO] http request received")
 }
 
 // Starts the webserver and exposes the different features on port 8080
 func main() {
-	router := http.NewServeMux()
+	router := mux.NewRouter()
 	router.HandleFunc("/", indexHandler)
 	router.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	log.Fatal(http.ListenAndServe(":8080", router))
